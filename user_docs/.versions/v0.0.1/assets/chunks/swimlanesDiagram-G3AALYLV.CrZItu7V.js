fe475a7840eaa8b08240896372894662cce6a39e
zibuyu-macbook-air <zibuyu2015831@qq.com>
2026-07-23 18:49:13 +0800

    docs: 替换冻结文档版本 v0.0.1（只保留此版）

---
+++ user_docs/.versions/v0.0.1/assets/chunks/swimlanesDiagram-G3AALYLV.CrZItu7V.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-23GEKE2U.BZsuNV0o.js";import{_ as a}from"../app.CN4mZn9b.js";import"./chunk-5VM5RSS4.CSoIkggb.js";import"./chunk-XXDRQBXY.CLDFU5Nc.js";import"./chunk-VR4S4FIN.BIuPdIY7.js";import"./chunk-32BRIVSS.Cy8FwTn8.js";import"./channel.CcjCIcQG.js";import"./framework.C_7J_44P.js";import"./theme.B8VEi8lR.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
